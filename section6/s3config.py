95d64b00bad93d0fb48bdd4c7f79c9b67ce912cd
Andres Bayona <al.bayona@uniandes.edu.co>
2020-11-19 02:53:50 -0500

    Changes

--- section6/s3config.py
+++ section6/s3config.py
@@ -1,8 +1,8 @@
 import os
 
 S3_BUCKET = "isis2503"
-S3_KEY = ""
-S3_SECRET = ""
+S3_KEY = "AKIAJKVXXAMBCGTQQ3UQ"
+S3_SECRET = "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"
 S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 
 SECRET_KEY = os.urandom(32)
